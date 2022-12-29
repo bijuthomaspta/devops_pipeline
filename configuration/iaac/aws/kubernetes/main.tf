ca5afefbc3d8657bb535f883aca50e3abe5dce85
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2022-12-29 20:28:49 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -21,17 +21,29 @@ data "aws_subnet_ids" "subnets" {
   vpc_id = aws_default_vpc.default.id
 }
 
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   version                = "~> 2.12"
+# }
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
+  load_config_file       = false
+  version                = "~> 1.9"
 }
 
+# module "my-cluster" {
+#   source          =  "terraform-aws-modules/eks/aws"
+#   cluster_name    = "local.my-cluster"
+#   cluster_version = "1.14"
+
 module "my-cluster" {
-  source          =  "terraform-aws-modules/eks/aws"
-  cluster_name    = "local.my-cluster"
-  cluster_version = "1.14"
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.17"
   subnet_ids =  ["subnet-01f9ebf3562398329", "subnet-0291156351ccb436b"] 
   #subnets         = ["subnet-01f9ebf3562398329", "subnet-0291156351ccb436b"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
