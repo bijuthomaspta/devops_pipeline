7d4f12d1d18ac100102e8d02710f3bf870022a62
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2022-12-29 22:31:19 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -51,8 +51,8 @@ module "my-cluster" {
 
   #vpc_id         = "vpc-1234556abcdef"
 
-  #node_groups = [
-  eks_managed_node_groups = [
+  node_groups = [
+#   eks_managed_node_groups = [
     {
       instance_type = "t2.micro"
       max_capacity  = 5
