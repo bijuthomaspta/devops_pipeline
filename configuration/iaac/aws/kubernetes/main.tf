b0e33d3640d8c3014840f5c5ed2e2b6b31adb014
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2022-12-29 23:04:10 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -52,14 +52,14 @@ module "my-cluster" {
   #vpc_id         = "vpc-1234556abcdef"
 
 #   node_groups = [
-  eks_managed_node_groups = [
-    {
+  eks_managed_node_groups = {
+    one = {
       instance_type = "t2.micro"
       max_capacity  = 5
       desired_capacity = 3
       min_capacity  = 3
     }
-  ]
+  }
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -92,5 +92,5 @@ resource "kubernetes_cluster_role_binding" "example" {
 
 # Needed to set the default region
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
