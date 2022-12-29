0dd0b0f97b4829c140ee8b790a0dc51be03092f8
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2022-12-30 00:50:37 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -17,44 +17,27 @@ resource "aws_default_vpc" "default" {
 
 }
 
-# data "aws_subnet_ids" "subnets" {
-#   vpc_id = aws_default_vpc.default.id
-# }
-
-data "aws_eks_cluster" "cluster" {
-  name = module.my-cluster.cluster_id
+data "aws_subnet_ids" "subnets" {
+  vpc_id = aws_default_vpc.default.id
 }
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.my-cluster.cluster_id
-}
-
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-#   load_config_file       = false
- # version                = "~> 1.21"
+  version                = "~> 2.12"
 }
 
-# module "my-cluster" {
-#   source          =  "terraform-aws-modules/eks/aws"
-#   cluster_name    = "local.my-cluster"
-#   cluster_version = "1.14"
-
-module "my-cluster" {
+module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
-#   cluster_version = "1.14"
-  subnet_ids = ["subnet-01f9ebf3562398329", "subnet-0291156351ccb436b"] 
-  #subnets         = ["subnet-01f9ebf3562398329", "subnet-0291156351ccb436b"] #CHANGE
+  cluster_name    = "in28minutes-cluster"
+  cluster_version = "1.14"
+  subnets         = ["subnet-01f9ebf3562398329", "subnet-0291156351ccb436b"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
   #vpc_id         = "vpc-1234556abcdef"
 
-#   node_groups = [
   eks_managed_node_groups = {
     one = {
       instance_type = "t2.micro"
@@ -62,9 +45,16 @@ module "my-cluster" {
       desired_capacity = 3
       min_capacity  = 3
     }
-  }
+  ]
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
+}
 
 
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
