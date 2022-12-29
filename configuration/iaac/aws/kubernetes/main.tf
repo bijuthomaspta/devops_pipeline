b6b691d1374252e0b25af6f5ae21ac4cdd18bcd5
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2022-12-29 13:55:21 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -25,18 +25,18 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
- # version                = "19.4.2"
+ # version                = "~> 2.12"
 }
 
 module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.14"
-  #subnets         = ["subnet-01f9ebf3562398329", "subnet-00d56bff72c265381"] #CHANGE
+  #subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
   subnets = data.aws_subnet_ids.subnets.ids
-  #vpc_id          = "vpc-0dcf29638688c0f1f"  #aws_default_vpc.default.id
+  vpc_id          = aws_default_vpc.default.id
 
-  vpc_id         = "vpc-1234556abcdef"
+  #vpc_id         = "vpc-1234556abcdef"
 
   node_groups = [
     {
