4d79dabc76f8e1f879138bdf92045fdd6d54928d
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2022-12-30 10:21:40 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -19,13 +19,12 @@ resource "aws_default_vpc" "default" {
 
 
 data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.cluster_id
+  name = module.my-cluster
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.cluster_id
+  name = module.my-cluster
 }
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
